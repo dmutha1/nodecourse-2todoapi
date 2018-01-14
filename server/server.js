1fa8ce4a795f86e3ad34c1336a48208e2544035d
Durga raj Mutha <dmutha1@outlook.com>
2018-01-13 18:59:23 -0600

    Mongoose Queries

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,29 @@ app.get('/todos', (req, res) =>{
   }, (e)=>{
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id',(req,res) =>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send('ID Invalid Empty Body');
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+    return res.status(404).send();
+    }
+
+return res.status(200).send(todo);
+}).catch((e)=>{
+  return res.status(400).send(id);
+});
+  //find by ID
+  //success case
+    //if todo - send it back
+
+  //error
+    //400 - send back nothing
 });
 
 
